a251f406ef46f0e4566682c05740443fa32e785d
Darshana120 <159713432+Darshana120@users.noreply.github.com>
2024-02-12 02:52:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/co
 
 
 //═══════[Required Variables]════════\\
-global.owner = process.env.OWNER_NUMBER || '94761516805'  // Make SURE its Not Be Empty, Else Bot Stoped And Errors,
+global.owner = process.env.OWNER_NUMBER || '94741262964'  // Make SURE its Not Be Empty, Else Bot Stoped And Errors,
 global.mongodb = process.env.MONGODB_URI || "mongodb+srv://SithumKalhara:97531@cluster0.iva7dbo.mongodb.net/?retryWrites=true&w=majority"
 global.port= process.env.PORT || 5000
 global.email = 'Sithumkalhara271@gmail.com'
@@ -15,12 +15,12 @@ global.devs = '94761516805';
 global.website = 'https://github.com/Sithuwa/SITHUWA-MD' //wa.me/+94000000000000
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/15b1dd8aeaa47888d75d7.jpg'
 module.exports = {
-  sessionName: process.env.SESSION_ID || "",      //Put Your Session Id Here
+  sessionName: process.env.SESSION_ID || "SITHUWA-MD;;;eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiV01yNlZpWU9wSGc1Y1BWUFhhYVgxaExCRVlrWVg5V2VKOUc0bVFNNDlGcz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiSG13dXBIQk1tRGlycTIrMXdwNzNSR3V0TGh2djhEU0w5VkdSY2VVeTV5TT0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiI4SXhRRENTQ2syL3Z2UjVZZEF1d2xKVlY4QWV2Mm1aU2pJV3BvRWwrWlVVPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJpZ21Rb2UzRko1R1MyaUpVNDgycDN6cWtqM2h6SWQyLzVzNkIrQkh3K0FjPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6InFKdEZscHQ4dUJoS2l0VUVhTUYzd1hBSWNkbXozODdkRW5jTXZtTGo4R1E9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImJWYm9MV0p3SDVQWUkzeDlpK05NeTVwNXl1Ym9ocWRpTFdDYi9JMVlZRnc9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiNkU2SlJEcXZzRnFyeklWcGpMQlI1R0dCOVErTGI4VDI4OUNlclNZR1gxOD0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiZG5xVGJDa1NoL0JweVkvSVd3bGpZZ24wdGtidDZUV3phYk9RbDRjNWdsQT0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IlNDOHRBZFQrbDBWVGJtQ1VVUXl1Y1Q3ekdwaVhSdkkwYmkwVi9nOXFXcjhua1dUY2hSMk5RMEhDZUtXMzBzcmpGU1NhRU1jYmtsUkpIMU80TnhrRURBPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6NywiYWR2U2VjcmV0S2V5IjoiemhCZytpUXpSVGp3ZjgwSno4cFRTVnVMNzNwQ1RTZkVWVjdYREk0Tm44UT0iLCJwcm9jZXNzZWRIaXN0b3J5TWVzc2FnZXMiOltdLCJuZXh0UHJlS2V5SWQiOjMxLCJmaXJzdFVudXBsb2FkZWRQcmVLZXlJZCI6MzEsImFjY291bnRTeW5jQ291bnRlciI6MCwiYWNjb3VudFNldHRpbmdzIjp7InVuYXJjaGl2ZUNoYXRzIjpmYWxzZX0sImRldmljZUlkIjoiVUd5U3JSQTRRTkd4TEpSYTFqVkdYUSIsInBob25lSWQiOiJjNTdjYzk1NS00MmMwLTQwNWQtOWMzMi1jMDkyOWYzMGMwNDUiLCJpZGVudGl0eUlkIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoia2lQYkk3amlMMmJtTW5qWmVQWnQxc1dXbkxZPSJ9LCJyZWdpc3RlcmVkIjpmYWxzZSwiYmFja3VwVG9rZW4iOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJjY0h6WGtLNWpidmhzTEhEYzIzdWpjQzNEMm89In0sInJlZ2lzdHJhdGlvbiI6e30sImFjY291bnQiOnsiZGV0YWlscyI6IkNNdTRsK01IRU0zdnBLNEdHQUVnQUNnQSIsImFjY291bnRTaWduYXR1cmVLZXkiOiI0OU43ZFVacTZ1U3orNncrSXJPbGx3RTRaWE56aHo0bTJqYjBpVHk4dmk4PSIsImFjY291bnRTaWduYXR1cmUiOiJNT2dzdC9RUXJhdmFPaHA3R2lrY1o0aDlJeG56RDMvcTNoakNPME9RTVQvNDF2dnhLOGhuLzl2YTZXNmkwN2NqOEU1eGUzNDNmeFhSOGdSdUFYMk1CQT09IiwiZGV2aWNlU2lnbmF0dXJlIjoiNmg3V1Bkbkg3eUNSdXlFT1lCK1BzZlN6N1VnY0U0MkpuZ1JRSkRITXZLYllsZ0dxeWQ4NnZqRndaV3liYTNFY1MreW5IMTNGamV3R1ZQa0t3cnNQRHc9PSJ9LCJtZSI6eyJpZCI6Ijk0NzQyNTI0NDAyOjRAcy53aGF0c2FwcC5uZXQiLCJuYW1lIjoiUyJ9LCJzaWduYWxJZGVudGl0aWVzIjpbeyJpZGVudGlmaWVyIjp7Im5hbWUiOiI5NDc0MjUyNDQwMjo0QHMud2hhdHNhcHAubmV0IiwiZGV2aWNlSWQiOjB9LCJpZGVudGlmaWVyS2V5Ijp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQmVQVGUzVkdhdXJrcy91c1BpS3pwWmNCT0dWemM0YytKdG8yOUlrOHZMNHYifX1dLCJwbGF0Zm9ybSI6InNtYmEiLCJsYXN0QWNjb3VudFN5bmNUaW1lc3RhbXAiOjE3MDc2ODU4NDAsIm15QXBwU3RhdGVLZXlJZCI6IkFBQUFBQXB5In0=",      //Put Your Session Id Here
   author:  process.env.PACK_AUTHER ||  'SITHUWA BOT',
   packname:  process.env.PACK_NAME || 'MADE BY SITHUM KALHARA',
   
   botname:   process.env.BOT_NAME === undefined ? "SITHUWA-MD" : process.env.BOT_NAME,
-  ownername: process.env.OWNER_NAME === undefined ? 'sithumkalhara' : process.env.OWNER_NAME,  
+  ownername: process.env.OWNER_NAME === undefined ? 'Darshana' : process.env.OWNER_NAME,  
   auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? false : process.env.AUTO_READ_STATUS,
   autoreaction:  process.env.AUTO_REACTION  === undefined ? false : process.env.AUTO_REACTION ,
   antibadword :  process.env.ANTI_BAD_WORD === undefined ? 'nbwoed' : process.env.ANTI_BAD_WORD,
